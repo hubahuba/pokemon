1c489f32b51d6f27ad9b83c04df4d73b3a0e6b9a
Utomo Tri Putranto <utomo_t_putranto@telkomsel.co.id>
2024-04-28 13:12:56 +0700

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,21 @@ module.exports = {
   preset: 'react-native',
   transformIgnorePatterns: ['/node_modules/(?!native-base)/'],
   setupFiles: ['./jest.setup.js'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'app/**/*.{ts,tsx}',
+    'core/**/*.{ts,tsx}',
+    '!app/**/*.d.{ts,tsx}',
+    '!core/**/*.d.{ts,tsx}',
+    '!core/definitions/**/*.{ts,tsx}',
+    '!app/routes/**/*.{ts,tsx}',
+    '!core/__tests__/constanta.ts',
+    '!core/utils/index.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      lines: 30,
+      functions: 30,
+    },
+  },
 };
--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,4 @@
 import 'react-native-gesture-handler/jestSetup';
+require('react-native-reanimated').setUpTests();
 
 jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
